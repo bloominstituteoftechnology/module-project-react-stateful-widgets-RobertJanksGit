3d85785ca5f7f00589a60c3c9e3a53d7ddbcb46a
Robert Jank <robert.jank@yahoo.com>
2024-06-19 22:17:45 -0500

    Final Push

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/reset.css'
-import './styles/styles.css'
-import Counter from './components/Counter';
-import Input from './components/Input';
-import Moods from './components/Moods';
-import Spinner from './components/Spinner';
-import Squares from './components/Squares';
-import Programmers from './components/Programmers';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/styles.css";
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+import Moods from "./components/Moods";
+import Spinner from "./components/Spinner";
+import Squares from "./components/Squares";
+import Programmers from "./components/Programmers";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 root.render(
   <>
@@ -21,4 +21,4 @@ root.render(
     <Squares />
     <Programmers />
   </>
-)
+);
